926dbd63e55fde79a7766ab0f9ecebbd96525fd2
chocobo9 <116327708+chocobo9@users.noreply.github.com>
2022-12-01 17:45:50 -0800

    Update disk_info.c
    finished.

--- csc360/p3/disk_info.c
+++ csc360/p3/disk_info.c
@@ -18,11 +18,13 @@ typedef struct diskinfo{
     char num_files[1];
     char num_FAT[1];
 }diskinfo;
+
 void get_os_name(char *os_name, char *p){
     for (int i = 0;i<8;i++){
         os_name[i] = p[i+3];
     }
 }
+
 void get_label(char *label,char *p){
     p+= 512*19;
     while(p[0]!= 0x00){
@@ -35,13 +37,62 @@ void get_label(char *label,char *p){
         p+= 32;
     }
 }
-void get_free_size(int st_size,char *byte_per_sec,char *free_size, char *p){
-    for(int i = 1; i<st_size/512; i++){
-        
+int get_total_sector(char *p){
+    return p[19]+(p[20]<<8);
+}//copy
+int get_sector_fat(char *p){
+    return p[22]+(p[23]<<8);
+}//copy
+
+int get_fat_val(int n, char *p){
+
+    int fourbit, eightbit;
+    if(n % 2 == 0){
+        fourbit = p[(n*3)/2+1]&0x0f;
+        eightbit = p[(n*3)/2]&0xff;
+        return (fourbit<<8)+eightbit;
+    }else{
+        fourbit = p[(n*3)/2]&0xf0;
+        eightbit = p[(n*3)/2+1]&0xff;
+        return (fourbit>>4)+(eightbit<<4);
+    } 
+}//copy
+
+int get_free_size(char *p){
+    int unused = 0;
+    int size = get_total_sector(p);
+    p+=512;
+
+    for(int i = 2; i<st_size/512; i++){
+        if(get_fat_val(i,p) == 0x00){
+            unused++;
+        }
     }
     p+= 512;
+    return unused * 512;
+}//copy
 
-}
+int get_file_num(char *p){
+    p+=512*19;
+    int count = 0;
+    for(int i = 0; i < 14*16 ; i++,p+=32){
+        if(p[11]==0x0f){
+            continue;
+        }
+        else if(((p[0]&0xff) == 0xE5) || ((p[0]&0xff) == 0x00)){
+            continue;
+        }
+        else if((p[26]+(p[27]<<8))<2){
+            continue;
+        }
+        else if(p[11] == 0x08){
+            continue;
+        }
+        count++;
+    }
+    return count;
+}//copy
+
 int main(int argc, char *argv[]){
     int fd = open(argv[1], O_RDWR);
     if(argc != 2){
@@ -68,8 +119,18 @@ int main(int argc, char *argv[]){
     //printf("total:%d\n",(p[11]+(p[12]<<8))*(p[19]+(p[20]<<8)));
     
     printf("Label of the disk: %s\n",di->lable);
-    get_free_size(buffer.st_size,di->byte_per_sec,di->free_size,p);
-    printf("Total size of the disk: %ld\n",buffer.st_size);
+    //get_free_size(buffer.st_size,di->byte_per_sec,di->free_size,p);
+    int freesize = get_free_size(p);
+    int filenum = get_file_num(p);
+    int fatNum = p[16];
+    int sectorFat = get_sector_fat(p);
+    printf("Total size of the disk: %ld bytes\n",buffer.st_size);
+    printf("Free size of the disk:%d bytes\n\n",freesize);
+    printf("==================\n");
+    printf("The number of files in the image: %d \n\n",filenum);
+    printf("==================\n");
+    printf("Number of fat copies:%d\n",fatNum);
+    printf("Sectors per Fat:%d\n", sectorFat);
     
     munmap(p, buffer.st_size); 
 	close(fd);
